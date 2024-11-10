ca54b172938e070868e7bc3afd21b8d48b3dd2fe
github_username_here <email@example.com>
2024-11-10 19:21:20 +1100

    Added wordcount function

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
+    file_path = "./books/frankenstein.txt"
+    book_text = read_text(file_path)
+    word_count = getwordcount(book_text)
+    print(book_text)
+    print(f"The word count is {word_count} words.")
+
+
+def getwordcount(book_text):
+    split_list = book_text.split()
+    new_list = []
+    count = 0
+    for word in split_list:
+        new_list.append(word)
+        count = count + 1
+    return count
+
+
+def read_text(file_path):    
+    with open(file_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 main()
-
