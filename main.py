2027cfeb92cf4ae631867aa2a20c541b60b9d9db
github_username_here <email@example.com>
2024-11-16 11:36:46 +1100

    Add unoptimised character count

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ def main():
     file_path = "./books/frankenstein.txt"
     book_text = read_text(file_path)
     word_count = getwordcount(book_text)
-    print(book_text)
-    print(f"The word count is {word_count} words.")
-
+    character_count = count_characters(book_text)
+   # print(book_text)
+ #   print(f"The word count is {word_count} words.")
+    print(character_count)
 
 def getwordcount(book_text):
     split_list = book_text.split()
@@ -20,5 +21,28 @@ def read_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
+
+
+def count_characters(book_text):
+    character_dict = {}
+    lowercase = book_text.lower()
+    split_list = lowercase.split()
+    staging_list = []
+    for word in split_list:
+        for character in word:
+            staging_list.append(character)
+            character_dict[character] = 0
+
+
+        for character in staging_list:    
+            character_dict[character] += 1
+    
+    
+    
+            
+        
+
+            
+    return character_dict
 
 main()
